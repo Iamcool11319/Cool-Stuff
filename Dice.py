6037989daafb90024297ac82868457e8d233576d
Iamcool11319 <ryanshao11@outlook.com>
2025-01-19 17:03:22 -0600

    Cool Stuff

--- Dice.py
+++ Dice.py
@@ -3,6 +3,6 @@ import random
 while True:
     roll = input("Roll? (y, n)\n")
     if roll == 'y':
-        print(random.randint(1, 7))
+        print(random.randint(1, 6))
     if roll == 'n':
         break
